107b4a68e1f99f5ec5a90e8d779e3108d02c9b52
Bobby de Vos <bobby_devos@sil.org>
2023-02-03 16:16:36 -0700

    Build fontforge less often

--- builds/fontforge/upgrade9.bash
+++ builds/fontforge/upgrade9.bash
@@ -1,12 +1,4 @@
-day=$(date | cut -d " " -f 1)
-if [ "$day" = "Mon" ]
-then
-    rm -rf build
-    #./bootstrap
-    #./configure --without-libzmq --enable-fontforge-extras --enable-python-scripting=3
-    #make
-    #sudo make install
-fi
+rm -rf build
 mkdir build
 pushd build
 cmake -GNinja -DENABLE_X11=ON -DENABLE_FONTFORGE_EXTRAS=ON ..
