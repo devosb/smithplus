5253921ce556c195ffd8b7c490d45b6daceac076
Bobby de Vos <bobby_devos@sil.org>
2025-01-30 10:10:18 -0700

    Improve testing

--- builds/harfbuzz/upgrade2-guest.bash
+++ builds/harfbuzz/upgrade2-guest.bash
@@ -1,5 +1,4 @@
 sudo rm -rf build
-mkdir build
 
 # On Ubuntu J... look at https://github.com/harfbuzz/harfbuzz/issues/3164#event-5302802120 for issues with ragel
 
@@ -7,14 +6,15 @@ mkdir build
 
 if [ -f meson.build ]
 then
-    meson setup build -Dgraphite=enabled -Dauto_features=enabled -Ddocs=disabled
+    meson setup build -Dgraphite2=enabled -Dauto_features=enabled -Ddocs=disabled -Dexperimental_api=true
     # meson setup build -Dauto_features=enabled -Dwasm=enabled -Ddocs=disabled
-    # meson compile -Cbuild -j9
-    ninja -Cbuild
-    meson test -Cbuild --print-errorlogs
-    pushd build
-    sudo meson install
+    meson compile -C build
+    # ninja -C build
+    meson test -C build --print-errorlogs
+    #pushd build
+    sudo meson install -C build
 else
+    mkdir build
     export NOCONFIGURE=true
     ./autogen.sh
     pushd build
@@ -22,7 +22,7 @@ else
     make
     sudo make install
     sudo cp ./src/*.typelib /usr/lib/girepository-1.0/
+    popd
 fi
 
-popd
 sudo ldconfig
--- builds/uharfbuzz/upgrade3.bash
+++ builds/uharfbuzz/upgrade3.bash
@@ -1,4 +1,5 @@
-python3 -m pip install --user .
+USE_SYSTEM_LIBS=1 pip3 install uharfbuzz --no-binary :uharfbuzz:
+# USE_SYSTEM_LIBS=1 python3 -m pip install --user .
 
 # python3 setup.py develop --user
 
