61dffda58f0a727f831867a01cf92228c0ef5856
Bobby de Vos <bobby_devos@sil.org>
2025-03-07 15:55:01 -0700

    Small, useful improvements

--- builds/ots/upgrade4.bash
+++ builds/ots/upgrade4.bash
@@ -5,10 +5,10 @@ export CXX=clang++
 rm -rf build
 #meson setup -Ddebug=true -Dgraphite=true build
 meson setup build
-ninja -C build
+meson compile -C build
 sudo meson install -C build
 
 # Reads all the fonts on my system,
 # in additonal to the fonts in the source tree,
 # so it take a long time to run.
-#ninja -C build test
+#meson test -C build
